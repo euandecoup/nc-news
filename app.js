6c382b877fcc2d604da48471cecc5586502e2b95
euandecoup <euandecoup@gmail.com>
2024-02-20 11:58:20 +0000

    4-getArticleById complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { getTopics } = require('./controllers/topics.controllers')
 const { getApis } = require('./controllers/api.controllers')
+const { getArticleById } = require('./controllers/articles.controllers')
 
 app.use(express.json())
 
@@ -9,15 +10,29 @@ app.get('/api/topics', getTopics)
 
 app.get('/api', getApis)
 
+app.get('/api/articles/:article_id', getArticleById)
+
+
 app.use((error, request, response, next) => {
     if (error.status && error.msg) {
         response.status(error.status)
         .send({msg: error.msg})
-    }
+    } else next(error)
+})
+
+app.use((error, request, response, next) => {
+    if (error.code === '22P02') {
+        response.status(400).send({msg: 'bad request'})
+    } else next(error)
 })
 
 app.all("/*", ((request, response, next)=>{
     response.status(404).send({msg: 'path not found'})
 }))
+
+app.use((error, request, response, next) => {
+    console.log(error)
+    response.status(500).send({msg: 'internal server error'})
+})
 
 module.exports = app
--- models/api.models.js
+++ models/api.models.js
@@ -1,3 +1,4 @@
+const app = require('../app')
 const endpoints = require('../endpoints.json')
 
 function selectAllApis() {
