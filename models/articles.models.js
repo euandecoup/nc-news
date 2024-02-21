c625185b90e4b84aa616409f4ee43335f1323962
euandecoup <euandecoup@gmail.com>
2024-02-21 08:47:33 +0000

    6-getCommentsByArticleId fully tested

--- models/articles.models.js
+++ models/articles.models.js
@@ -31,4 +31,18 @@ function fetchAllArticles() {
         })
 }
 
-module.exports = { fetchArticleById, fetchAllArticles }
+function fetchAllCommentsByArticleId(id) {
+    return db.query(`
+        SELECT comment_id, votes, created_at, author, body, article_id
+        FROM comments
+        WHERE article_id=$1
+        ORDER BY created_at DESC`, [id])
+        .then(({rows}) => {
+            if (rows.length === 0) {
+                return Promise.reject({status: 404, msg: 'comments not found'})
+            }
+            return rows
+        })
+}
+
+module.exports = { fetchArticleById, fetchAllArticles, fetchAllCommentsByArticleId }
